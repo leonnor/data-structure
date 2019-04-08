a551382ebbd31e0e36cae6423c116298fe2e5371
leonnor <958978379@qq.com>
2019-04-08 17:15:48 +0800

    LinkedList

--- LinkedList/src/LinkedList.java
+++ LinkedList/src/LinkedList.java
@@ -31,11 +31,11 @@ public class LinkedList<E> {
         }
     }
 
-    private Node head;
+    private Node dummyHead;
     private int size;
 
     public LinkedList(){
-        head = null;
+        dummyHead = new Node(null, null);
         size = 0;
     }
 
@@ -47,14 +47,7 @@ public class LinkedList<E> {
         return size == 0;
     }
 
-    /**
-     * 在链表头添加元素
-     * @param e
-     */
-    public void addFirst(E e){
-        head = new Node(e, head);
-        size++;
-    }
+
 
     /**
      * 在链表的index（0-based）位置添加新元素e
@@ -65,20 +58,123 @@ public class LinkedList<E> {
         if(index < 0 || index > size){
             throw new IllegalArgumentException("Add failed. Illegal index.");
         }
-
-        if (index == 0){
-            addFirst(e);
-        }else {
-            Node prev = head;
-            for (int i = 0; i < index - 1; i++){
-                prev = prev.next;
-            }
-            prev.next = new Node(e, prev.next);
-            size++;
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++){
+            prev = prev.next;
         }
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+    /**
+     * 在链表头添加元素
+     * @param e
+     */
+    public void addFirst(E e){
+        add(0, e);
     }
 
+    /**
+     * 链表尾添加元素
+     * @param e
+     */
     public void addLast(E e){
         add(size, e);
+    }
+
+    /**
+     * 获得链表的index（0-based）位置的元素e
+     * @param index
+     * @return
+     */
+    public E get(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Get failed. Illegal index.");
+        }
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++){
+            cur = cur.next;
+        }
+        return cur.e;
+    }
+
+    public E getFirst(){
+        return get(0);
+    }
+
+    public E getLast(){
+        return get(size - 1);
+    }
+
+    /**
+     * 修改链表的index（0-based）位置的元素为e
+     * @param index
+     * @param e
+     */
+    public void set(int index, E e){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Set failed. Illegal index.");
+        }
+
+        Node cur = dummyHead.next;
+        for (int i = 0; i < index; i++){
+            cur = cur.next;
+        }
+        cur.e = e;
+    }
+
+    /**
+     * 查找链表中是否有元素e
+     * @param e
+     * @return
+     */
+    public boolean contains(E e){
+        Node cur = dummyHead.next;
+        while(cur != null){
+            if (cur.e.equals(e)){
+                return true;
+            }
+            cur = cur.next;
+        }
+        return false;
+    }
+
+    /**
+     * 删除index位置的元素，返回删除的元素
+     * @param index
+     * @return
+     */
+    public E remove(int index){
+        if (index < 0 || index >= size){
+            throw new IllegalArgumentException("Remove failed. Illegal index.");
+        }
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++){
+            prev = prev.next;
+        }
+        Node retNode = prev.next;
+        prev.next = retNode.next;
+        retNode.next = null;
+        size--;
+        return retNode.e;
+    }
+
+    public E removeFirst(){
+        return remove(0);
+    }
+
+    public E removeLast(){
+        return remove(size - 1);
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder res = new StringBuilder();
+        Node cur = dummyHead.next;
+        while (cur != null){
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL");
+        return res.toString();
     }
 }
