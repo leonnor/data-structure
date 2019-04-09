fb7e8dfa89df7e106c75c62c7b7694ec3b54166c
leonnor <958978379@qq.com>
2019-04-09 20:42:24 +0800

    LinkedList

---
+++ LinkedList/src/ListNode.java
@@ -0,0 +1,14 @@
+/**
+ * className ListNode
+ * description TODO
+ *
+ * @author ln
+ * @version 1.0
+ * @date 2019/4/9 17:24
+ */
+
+public class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) { val = x; }
+}
