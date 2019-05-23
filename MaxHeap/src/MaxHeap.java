01001c2bcdd59cea6cfd88af3114e0067cf6eb29
leonnor <958978379@qq.com>
2019-05-23 15:29:08 +0800

    MaxHeap

--- MaxHeap/src/MaxHeap.java
+++ MaxHeap/src/MaxHeap.java
@@ -26,5 +26,50 @@ public class MaxHeap<E extends Comparable<E>> {
         return data.isEmpty();
     }
 
+    /**
+     * 返回完全二叉树的数组表示中，一个索引表示的元素的父亲节点的索引
+     * @param index
+     * @return
+     */
+    private int parent(int index){
+        if (index == 0){
+            throw new IllegalArgumentException("index-0 doesn't have parent");
+        }
+        return (index - 1) / 2;
+    }
 
+    /**
+     * 返回完全二叉树的数组表示中，一个索引表示的元素的左孩子节点的索引
+     * @param index
+     * @return
+     */
+    private int  leftChild(int index){
+        return index * 2 + 1;
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引表示的元素的右孩子节点的索引
+     * @param index
+     * @return
+     */
+    private int rightChild(int index){
+        return index * 2 + 2;
+    }
+
+    /**
+     * 向堆中添加元素
+     * @param e
+     */
+    public void add(E e){
+        data.addLast(e);
+        siftUp(data.getSize() - 1);
+    }
+
+    private void siftUp(int k){
+
+        while(k > 0 && data.get(parent(k)).compareTo(data.get(k)) < 0){
+            data.swap(k, parent(k));
+            k = parent(k);
+        }
+    }
 }
