a1f50364f56e00379eac8efe7e7b40749b28f368
leonnor <958978379@qq.com>
2019-05-24 14:47:08 +0800

    MaxHeap

--- MaxHeap/src/MaxHeap.java
+++ MaxHeap/src/MaxHeap.java
@@ -18,6 +18,13 @@ public class MaxHeap<E extends Comparable<E>> {
         data = new Array<>();
     }
 
+    public MaxHeap(E[] arr){
+        data = new Array<>();
+        for(int i = parent(arr.length - 1); i >= 0; i--){
+            siftDown(i);
+        }
+    }
+
     public int size() {
         return data.getSize();
     }
@@ -117,4 +124,19 @@ public class MaxHeap<E extends Comparable<E>> {
             k = j;
         }
     }
+
+    /**
+     * 取出堆中最大元素， 并且替换成元素e
+     * @param e
+     * @return
+     */
+    public E replace(E e){
+
+        E ret = findMax();
+        data.set(0, e);
+        siftDown(0);
+        return ret;
+    }
+
+
 }
